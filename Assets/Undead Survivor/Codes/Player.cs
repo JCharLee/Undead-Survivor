b00f7a17d95cea017da380574181bbf3b7fba4bb
JCharLee <wotjd999@gmail.com>
2024-11-13 21:28:56 +0900

    프로젝트 이동
    기존 프로젝트 깃허브에 업로드.

---
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+    public Scanner scanner;
+    public Hand[] hands;
+    public RuntimeAnimatorController[] animCon;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        scanner = GetComponent<Scanner>();
+        hands = GetComponentsInChildren<Hand>(true);
+    }
+
+    void OnEnable()
+    {
+        speed *= Character.Speed;
+        anim.runtimeAnimatorController = animCon[GameManager.instance.playerId];
+    }
+
+    //void Update()
+    //{
+    //    inputVec.x = Input.GetAxisRaw("Horizontal");
+    //    inputVec.y = Input.GetAxisRaw("Vertical");
+    //}
+
+    void FixedUpdate()
+    {
+        if (!GameManager.instance.isLive)
+            return;
+
+        #region [물리 기본 움직임]
+        //Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+
+        // 1. 힘을 준다
+        //rigid.AddForce(inputVec);
+
+        // 2. 속도 제어
+        //rigid.velocity = inputVec;
+
+        // 3. 위치 이동
+        //rigid.MovePosition(rigid.position + nextVec);
+        #endregion
+
+        // 인풋 시스템 움직임
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        if (!GameManager.instance.isLive)
+            return;
+
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!GameManager.instance.isLive)
+            return;
+
+        GameManager.instance.health -= Time.deltaTime * 10;
+
+        if (GameManager.instance.health < 0)
+        {
+            for (int index = 2; index < transform.childCount; index++)
+            {
+                transform.GetChild(index).gameObject.SetActive(false);
+            }
+
+            anim.SetTrigger("Dead");
+            GameManager.instance.GameOver();
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+}
